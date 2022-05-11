aea3a7825e876f7b0d01847c0a6cd05b7d7eb85c
SamRobertsArup <81683795+SamRobertsArup@users.noreply.github.com>
2022-05-11 17:42:32 +0100

    Create koffee.py

---
+++ koffee.py
@@ -0,0 +1,28 @@
+import pyautogui
+import datetime
+import time
+import os
+
+cmd = 'mode 30,10'
+os.system(cmd)
+print("Koffee keeps your computer awake whilst you sleep")
+val = input("Enter shutdown'o'clock? (hh:mm)\n")
+minsIdle = 0
+
+pyautogui.PAUSE = 0.5
+while True:
+    x, y = pyautogui.position()
+    time.sleep(60)
+    curX, curY = pyautogui.position()
+    if (x, y) == (curX, curY):
+        minsIdle +=1
+        pyautogui.press("shift")
+        pyautogui.move(1,0)
+        pyautogui.move(-1,0)
+        print(str(datetime.datetime.now())+" "+str(minsIdle))
+    else:
+        minsIdle = 0
+        
+    if str(datetime.datetime.now().strftime("%H:%M")) == val:  #"17:30":
+        print("works over!!")
+        os.system("shutdown -s")
